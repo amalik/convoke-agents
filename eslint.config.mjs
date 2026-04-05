38c2253c26b5256bd42c73585fd6fd51364f0cc4
Amalik Amriou <amalik.amriou@gmail.com>
2026-04-06 00:33:07 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,7 @@ export default [
       }
     },
     rules: {
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }],
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }],
       "no-undef": "error",
       "no-constant-condition": ["error", { "checkLoops": false }]
     }
